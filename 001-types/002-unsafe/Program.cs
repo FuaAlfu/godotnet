40c1f00c6cc5b07046a05ad49493bc0f1f36862c
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-06-14 22:46:27 +0300

    add

---
+++ 001-types/002-unsafe/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _002_unsafe
+{
+   internal unsafe class Program {  
+    private static void Main() {  
+        unsafe {  
+            int a = 20;  
+  
+            int * ptr = & a;  
+  
+            int * ptrptr2 = ptr;  
+            Console.WriteLine($ "Value of a is {*ptr}");  
+            Console.WriteLine($ "Address of a is {(int)ptr2}");  
+        }  
+    }  
+} 
+}
---
+++ 001-types/003-Dictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _003_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             	IDictionary<string, int> lich = new Dictionary<string, int>();
+	 	lich.Add("One",22); 
+	 	lich.Add("Three",33);
+		
+		foreach(KeyValuePair<string, int> kv in lich)
+        {
+            Console.WriteLine("the key: {0}, the value: {1}", kv.Key, kv.Value);
+        }
+		    Console.WriteLine("---");
+	// 	more..
+		var games = new Dictionary<string, string>(){
+			{"Dark Souls","hope grif die live"},
+			{"Elder Scroll","glitches, more glitches"},
+		};
+
+             foreach(var kv in games)
+           {
+               Console.WriteLine("the key: {0}, the value: {1}", kv.Key, kv.Value);
+           }
+          Console.ReadLine();
+        }
+    }
+}
